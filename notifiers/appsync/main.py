e9a6fcf31a52f00b9f2c608a6eac8f0b40ffbbd8
Nikhil Anand <34477129+NikSWE@users.noreply.github.com>
2023-12-20 20:08:12 -0500

    add secrets fetch function

--- notifiers/appsync/main.py
+++ notifiers/appsync/main.py
@@ -5,6 +5,7 @@ import os
 import requests
 import json
 import requests
+import boto3
 
 def fetch_current_forex_prices(url, q):
     r = redis.Redis(
@@ -22,6 +23,23 @@ def fetch_current_forex_prices(url, q):
         val = r.get(name)
         if val is not None:
             q.put({'name': name,'data': val})
+
+def get_from_secrets(SecretId, key):
+    session = boto3.session.Session()
+    sm = session.client(
+        service_name = 'secretsmanager',
+        region_name = 'us-east-1',
+        aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    )
+
+    value = json.loads(
+        sm.get_secret_value(
+            SecretId=SecretId
+            )['SecretString']
+        )[key]
+
+    return value
 
 def perform_mutations_appsync(api_key, url, q):
     while True:
@@ -49,13 +67,14 @@ def perform_mutations_appsync(api_key, url, q):
 if __name__ == '__main__':
     load_dotenv()
     q = Queue()
-    api_key = os.getenv("API_KEY_APPSYNC")
-    redis_url = os.getenv("REDIS_URL")
-    appsync_url = os.getenv("APPSYNC_URL")
+    api_key = get_from_secrets('appsync-details', 'key')
+    redis_url = get_from_secrets('redis-server', 'ip')
+    appsync_url = get_from_secrets('appsync-details', 'url')
     p1 = Process(target=fetch_current_forex_prices, args=(redis_url, q,))
     p2 = Process(target=perform_mutations_appsync, args=(api_key, appsync_url, q,))
     p1.start()
     p2.start()
     p1.join()
     p2.join()
+
 
