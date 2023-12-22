c73ef71f9bab6bde9ed9f1efdf6cc22a93c3ac3f
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2023-12-22 06:51:24 -0500

    updated code to es 6.7

--- backend/assets/news-summarizer-sagemaker-lambda-deployment/news_summarizer_sagemaker_lambda.py
+++ backend/assets/news-summarizer-sagemaker-lambda-deployment/news_summarizer_sagemaker_lambda.py
@@ -1,18 +1,67 @@
+import boto3
 import json
 from eventregistry import *
-import boto3
-import logging
-import urllib
 from opensearchpy import OpenSearch, RequestsHttpConnection
+from botocore.exceptions import ClientError
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+#secret_name = 'NewsSummaryMasterUserCA21B9-QkXben5lAXO2'
+secret_name = 'NewsSummaryMasterUserCA21B9-3S9eiHVsGh8K'
+region_name = 'us-east-1'
 
+session = boto3.session.Session()
+client = session.client(
+    service_name='secretsmanager',
+    region_name=region_name
+)
+
+try:
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
+    )
+    user = json.loads(get_secret_value_response['SecretString'])
+except ClientError as e:
+    print(f'failed to fetch secret: {e}')
+    raise e
+
+print(user)
+
+username = user['username']
+password = user['password']
+
+print(username, password)
+
+elasticsearch_endpoint = 'search-news-summary-adz52g6kcerbztcrc4v33ehclm.us-east-1.es.amazonaws.com'
+
+
+elasticsearch_client = OpenSearch(
+				hosts=[{'host':elasticsearch_endpoint ,'port': 443}], 
+				http_auth=(username, password),
+				use_ssl = True, 
+				verify_certs=True
+		)
+
+# TODO: replace
+index_name = 'tests'
+
+def save_to_es(news_url, summary, id_news, title):
+    print('Saving News to open Search')
+    body = {
+      'summary': summary,
+      'key': id_news,
+      'createdTimestamp': str(int(time.time())),
+      'url': news_url,
+      'title': title
+      
+    }
+    ret = elasticsearch_client.index(index=index_name, id=id_news, body=body, refresh = True)
+    print('Data saved:', ret)
+    return
 
 def sagemaker_inference(input_data):
     input_payload = {
         'inputs': input_data
     }
+
     
     response = sm_runtime.invoke_endpoint(
         EndpointName=endpoint_name,
@@ -23,52 +72,30 @@ def sagemaker_inference(input_data):
     
     return result["summary"]
 
-
-def save_to_es(news_url, summary, id_news):
-    print('Saving News to open Search')
-    body = {
-      'summary': summary,
-      'key': id_news,
-      'createdTimestamp': str(int(time.time())),
-      'url': news_url,
-      
-    }
-    ret = os.index(index="tests", id=id_news, body=body, refresh = True)
-    print('NLPA -- ES Push response:', ret)
-    return
-
-os = OpenSearch(
-				hosts=[{'host': 'search-news-summary-77kckz65rityx5xplsi5x72ss4.us-east-1.es.amazonaws.com','port': 443}], 
-				http_auth=('master', 'PC=0#8rvAn.g6S;Vj,gZ70VLS~qNBqx8'), 
-				use_ssl = True, 
-				verify_certs=True, 
-				connection_class=RequestsHttpConnection
-		)
-
 er = EventRegistry(apiKey = "adf8be6c-1510-4383-807b-388821552ce8")
-
 sm_runtime = boto3.client('sagemaker-runtime')
-endpoint_name = 'huggingface-pytorch-inference-2023-12-13-19-20-06-220'
+endpoint_name = 'huggingface-pytorch-inference'
 
 usUri = er.getLocationUri("USA")   
 
 def handler(event, context):
     q = QueryArticlesIter(
-        keywords = QueryItems.OR(["Business News"]),
+        keywords = QueryItems.OR(["Stock news"]),
         minSentiment = 0.4,
         sourceLocationUri = usUri,
         dataType = ["news", "blog"])
 
 
-    for art in q.execQuery(er, sortBy = "date", maxItems = 5):
+    for art in q.execQuery(er, sortBy = "date", maxItems = 50):
+        title = art['title']
         news = art["body"]
         news_url = art["url"]
         id_news = art['uri']
         summary = sagemaker_inference(news)
-        save_to_es(news_url, summary, id_news)
-        print(summary)
+        save_to_es(news_url, summary, id_news, title)
+
 
     return {
         'statusCode': 200,
-        'body': json.dumps(summary)
+        'body': json.dumps("News Updated")
     }
